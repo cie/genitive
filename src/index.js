55a3fed16136147487135bc33177254b91c51874
Bernát Kalló <kallo.bernat@gmail.com>
2020-07-30 12:11:43 +0200

    feat: filter sets

--- src/index.js
+++ src/index.js
@@ -3,25 +3,37 @@ module.exports = function (code) {
     if (typeof v === 'string') {
       const m = /<(?:(.*):)?([^>]+)>/.exec(v)
       if (!m) return void (yield v)
-      const filters = m[1] === undefined ? [] : m[1].split(/:(?!=)/)
-      for (const filter of filters)
-        if (!(env = applyFilter(filter, env))) return
+      const filterSets = m[1] === undefined ? [] : m[1].split(/:(?!=)/)
       const end = m.index + m[0].length
       const head = v.slice(0, m.index)
       const key = m[2]
       if (!(key in code)) throw new Error(`<${key}> is undefined`)
-      for (const substitution of expand(code[key], env))
-        for (const tail of expand(v.slice(end), env))
-          yield `${head}${substitution}${tail}`
+      for (const newEnv of applyFilterSets(filterSets, env))
+        for (const substitution of expand(code[key], newEnv))
+          for (const tail of expand(v.slice(end), env))
+            yield `${head}${substitution}${tail}`
     } else if (Array.isArray(v)) {
       for (const elem of v) for (const result of expand(elem, env)) yield result
     } else if (typeof v === 'object' && v !== null) {
       for (const [filter, w] of Object.entries(v)) {
-        const newEnv = applyFilter(filter, env)
-        if (!newEnv) continue
-        for (const result of expand(w, newEnv)) yield result
+        for (const newEnv of applyFilterSet(filter, env))
+          for (const result of expand(w, newEnv)) yield result
       }
     } else throw new Error('invalid value: ' + JSON.stringify(v))
+  }
+}
+
+function * applyFilterSets (filterSets, env) {
+  if (filterSets.length === 0) yield env
+  else
+    for (const e of applyFilterSet(filterSets[0], env))
+      for (const newEnv of applyFilterSets(filterSets.slice(1), e)) yield newEnv
+}
+
+function * applyFilterSet (filterSet, env) {
+  for (const filter of filterSet.split('|')) {
+    const newEnv = applyFilter(filter, env)
+    if (newEnv) yield newEnv
   }
 }
 
