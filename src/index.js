4eed434a55ec342b3ef762f8c75e808d7d39b048
Bernát Kalló <kallo.bernat@gmail.com>
2020-07-30 11:21:51 +0200

    feat: inline filters

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 module.exports = function (code) {
   return function * expand (v, env = {}) {
     if (typeof v === 'string') {
-      const m = /<([^>]+)>/.exec(v)
+      const m = /<(?:(.*):)?([^>]+)>/.exec(v)
       if (!m) return void (yield v)
       const end = m.index + m[0].length
       const head = v.slice(0, m.index)
-      const key = m[1]
+      const filters = m[1] === undefined ? [] : m[1].split(':')
+      for (const filter of filters) {
+        if (!(env = applyFilter(filter, env))) return
+      }
+      const key = m[2]
       if (!(key in code)) throw new Error(`<${key}> is undefined`)
       for (const substitution of expand(code[key], env))
         for (const tail of expand(v.slice(end), env))
@@ -14,38 +18,35 @@ module.exports = function (code) {
       for (const elem of v) for (const result of expand(elem, env)) yield result
     } else if (typeof v === 'object' && v !== null) {
       for (const [filter, w] of Object.entries(v)) {
-        const m = /^(.*?)\s*(=|:=|!=|<=|<|>=|>|\+=|-=|\*=|\?=)\s*(.*)$/.exec(
-          filter
-        )
-        if (!m) throw new Error('invalid filter: ' + filter)
-        const [_, variable, operator, value] = m
-        if (!checkVar(variable, operator, value, env)) continue
-        const newEnv = assignVar(variable, operator, value, env)
+        const newEnv = applyFilter(filter, env)
+        if (!newEnv) continue
         for (const result of expand(w, newEnv)) yield result
       }
     } else throw new Error('invalid value: ' + JSON.stringify(v))
   }
 }
 
-function checkVar (variable, operator, value, env) {
-  if (operator === ':=' || operator === '?=') return true
-  if (operator === '!=') return !(variable in env) || env[variable] !== value
-  if (operator == '=') return !(variable in env) || env[variable] == value
-  if (!(variable in env)) throw new Error(`${variable} has no value`)
-  const current = env[variable]
-  if (operator === '<') return current < value
-  if (operator === '<=') return current <= value
-  if (operator === '>') return current > value
-  if (operator === '>=') return current >= value
-  return true
-}
-
-function assignVar (variable, operator, value, env) {
-  if (operator === ':=' || operator == '=') return { ...env, [variable]: value }
+function applyFilter (filter, env) {
+  const m = /^(.*?)\s*(=|:=|!=|<=|<|>=|>|\+=|-=|\*=|\?=)\s*(.*)$/.exec(filter)
+  if (!m) throw new Error('invalid filter: ' + filter)
+  const [_, variable, operator, value] = m
+  if (operator === ':=') return { ...env, [variable]: value }
   if (operator === '?=')
     return variable in env ? env : { ...env, [variable]: value }
-  if (operator === '+=') return { ...env, [variable]: +env[variable] + +value }
-  if (operator === '-=') return { ...env, [variable]: env[variable] - value }
-  if (operator === '*=') return { ...env, [variable]: env[variable] * value }
-  return env
+  if (operator === '!=')
+    return (!(variable in env) || env[variable] !== value) && env
+  if (operator === '=')
+    return !(variable in env)
+      ? { ...env, [variable]: value }
+      : env[variable] == value && env
+  if (!(variable in env)) throw new Error(`${variable} has no value`)
+  const current = env[variable]
+  if (operator === '<') return current < value && env
+  if (operator === '<=') return current <= value && env
+  if (operator === '>') return current > value && env
+  if (operator === '>=') return current >= value && env
+  if (operator === '+=') return { ...env, [variable]: +current + +value }
+  if (operator === '-=') return { ...env, [variable]: current - value }
+  if (operator === '*=') return { ...env, [variable]: current * value }
+  throw new Error(`invalid operator: ${operator}`)
 }
